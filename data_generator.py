f886081f58162a729918279e65e08201acef8079
Wojciech Panfil <wojtek.panfil@wp.pl>
2022-12-02 11:53:07 +0100

    Fix entering from keyboard not working

--- data_generator.py
+++ data_generator.py
@@ -24,7 +24,7 @@ def generate_fake_records(number_of_records=10):
 
 def enter_records():
     number_of_records = int(input("Please enter how many records you want to create:\n"))
-    with open("data.txt", "w") as file:
+    with open("tape3.txt", "w") as file:
         for _ in range(number_of_records):
             record = Record(float(input("Enter probability of event a")), float(input("Enter probability of event b")),
                             float(input("Enter probability of sum of events")))
